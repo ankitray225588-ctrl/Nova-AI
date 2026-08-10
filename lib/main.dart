9dc32b3c97d9fa4bb0d49c800abc1a9f216733c1
ankitray225588-ctrl <ankitray225588@gmail.com>
2026-08-10 14:47:56 +0530

    Add flutter_tts package import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
-
+import 'package:flutter_tts/flutter_tts.dart';
 void main() {
   runApp(const NovaAI());
 }
